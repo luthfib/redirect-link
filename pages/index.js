724fdd1cd7c34d3fccfea72e68584af776722e69
Sarmun Bustillo <sarmun.b@gmail.com>
2022-06-16 15:27:43 +0200

    try adding download

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 export default function Home() {
-  return (
-    <a href="https://survey.napier.ac.uk/n/Kali2024.aspx" target="_blank">
-      Open Link Survey
-    </a>
-  );
+    return (
+        <a
+            href="https://survey.napier.ac.uk/n/Kali2024.aspx"
+            target="_blank"
+            download
+        >
+            Open Link Survey
+        </a>
+    );
 }
