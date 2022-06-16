b2f1ad7e23dfcb42c40c52951401f9e2ca21b300
Sarmun Bustillo <sarmun.b@gmail.com>
2022-06-16 19:00:22 +0200

    Try window.location.assing

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,17 @@ export default function Home() {
             );
         }
     }
+    function handleClick2() {
+        if (typeof window !== 'undefined') {
+            window.document.location.assign(
+                'https://survey.napier.ac.uk/n/Kali2024.aspx'
+            );
+        }
+    }
     return (
         <>
             <button onClick={handleClick}>Button open</button>
+            <button onClick={handleClick2}>Button open 2</button>
             <a
                 href="https://survey.napier.ac.uk/n/Kali2024.aspx"
                 target="_blank"
