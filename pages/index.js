4b0f03ddf344869889e3f11cd72f717311f47d98
Sarmun Bustillo <sarmun.b@gmail.com>
2022-06-16 19:18:06 +0200

    test browser

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
+import { useState } from 'react';
+
 export default function Home() {
+    const [userData, setuserData] = useState('');
+    const [onInstagram, setOnIstagram] = useState(false);
+    const [onFacebook, setFnFacebook] = useState(false);
     function handleClick() {
-        if (typeof window !== 'undefined') {
-            window.open(
-                'https://survey.napier.ac.uk/n/Kali2024.aspx',
-                '_system'
-            );
+        console.log(navigator.userAgent);
+        setuserData(navigator.userAgent);
+        if (userData.indexOf('Instagram') !== -1) {
+            setOnIstagram(true);
+        }
+        if (userData.indexOf('Facebook') !== -1) {
+            setFnFacebook(true);
         }
     }
-    function handleClick2() {
-        if (typeof window !== 'undefined') {
-            window.document.location.assign(
-                'https://survey.napier.ac.uk/n/Kali2024.aspx'
-            );
-        }
-    }
+
     return (
         <>
-            <button onClick={handleClick}>Button open</button>
-            <button onClick={handleClick2}>Button open 2</button>
+            <button onClick={handleClick}>userData</button>
+
             <a
                 href="https://survey.napier.ac.uk/n/Kali2024.aspx"
                 target="_blank"
             >
                 Open Link Survey
             </a>
+            <h2>userAgent:{userData}</h2>
+            <br />
+            <h3>On instagram? {`${onInstagram}`}</h3>
+            <h3>On facebook? {`${onFacebook}`}</h3>
         </>
     );
 }
