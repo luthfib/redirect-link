6af2b63694d36e3bdc046018bf0e5f4f398f8914
Sarmun Bustillo <sarmun.b@gmail.com>
2022-06-16 18:32:33 +0200

    Button test

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 export default function Home() {
+    function handleClick() {
+        if (typeof window !== 'undefined') {
+            window.open(
+                'https://survey.napier.ac.uk/n/Kali2024.aspx',
+                '_blank'
+            );
+        }
+    }
     return (
-        <a
-            href="https://survey.napier.ac.uk/n/Kali2024.aspx"
-            target="_blank"
-            download
-        >
-            Open Link Survey
-        </a>
+        <>
+            <button onClick={handleClick}>Button open</button>
+            <a
+                href="https://survey.napier.ac.uk/n/Kali2024.aspx"
+                target="_blank"
+            >
+                Open Link Survey
+            </a>
+        </>
     );
 }
